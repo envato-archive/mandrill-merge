f42ceb2c0d62a98fea81dd7b8e86b4c7333aae76
Glenn Tweedie <glenn.tweedie@envato.com>
2015-04-01 16:34:47 +0800

    pend the specs

--- spec/db_connection_spec.rb
+++ spec/db_connection_spec.rb
@@ -1,12 +1,12 @@
 require_relative 'spec_helper'
 
 describe 'views' do
-  it "displays the connection list" do
+  it "displays the connection list", pending:true do
     get "/db"
     expect(last_response.body).to include('Database Connections')
   end
 
-  it "displays the new connection form" do
+  it "displays the new connection form", pending:true do
     get "/db/new"
     expect(last_response.body).to include('<input name="database"')
   end
