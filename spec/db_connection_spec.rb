212f64694c804f4c375dda673bf92c5ef8212ade
Glenn Tweedie <glenn.tweedie@envato.com>
2015-04-01 12:03:27 +0800

    add basic spec for db form

--- spec/db_connection_spec.rb
+++ spec/db_connection_spec.rb
@@ -1,8 +1,13 @@
 require_relative 'spec_helper'
 
-describe 'view' do
+describe 'views' do
   it "displays the connection list" do
     get "/db"
     expect(last_response.body).to include('Database Connections')
+  end
+
+  it "displays the new connection form" do
+    get "/db/new"
+    expect(last_response.body).to include('<input name="database"')
   end
 end
