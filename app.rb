7c63fd71e4f4d6427c08ae16118ecd79db72755e
Mary-Anne Cosgrove <mary-anne.cosgrove@envato.com>
2015-04-02 15:47:16 +1100

    Madjectives!

--- app.rb
+++ app.rb
@@ -23,3 +23,4 @@ end
 
 require_relative 'assets'
 require_relative 'routes'
+require_relative 'helpers'
---
+++ helpers.rb
@@ -0,0 +1,8 @@
+
+MADJECTIVES = ["Macabre", "Mad", "Magic", "Magnificent", "Majestic", "Marvellous", "Masterful", "Meaty", "Mechanical", "Mechanised", "Mellifluous", "Meretricious", 
+  "Meritorious", "Metallic", "Mighty", "Militant", "Minimal", "Modish", "Molten", "Monomorphic", "Mountainous", "Multiparous", "Munificent", "Musical", "Myopic", 
+  "Myriad", "Mysterious"]
+
+def madjective
+  MADJECTIVES.sample
+end
