c443a91aa0c1d2090539ca837683bf8ddcee0e80
Steven Douglas <steven.douglas@envato.com>
2015-04-02 16:10:10 +1100

    Roll our on ajax forms and json response handling

--- app.rb
+++ app.rb
@@ -19,6 +19,31 @@ class App < Sinatra::Application
 
   enable :sessions
 
+  get '/' do
+    redirect '/mail-merge'
+  end
+
+  get '/mail-merge' do
+    erb :mail_merge
+  end
+
+  post '/verify-mandrill' do
+    session[:key] = params[:key]
+    mandrill = Mandrill.new(session[:key])
+
+    if mandrill.can_connect?
+      message = "Connection successful: User is #{mandrill.username}"
+    else
+      message = 'Connection unsuccessful'
+    end
+
+    {:can_connect => mandrill.can_connect?, :message => message}.to_json
+  end
+
+  get '/docs' do
+    erb :index
+  end
+
 end
 
 require_relative 'assets'
--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -1,3 +1,31 @@
+$.ajaxSetup ({
+   cache: false
+});
+
 jQuery(function() {
   $(document).foundation();
-}); 
+
+  $('form[ajax-form]').submit(function(e) {
+    var action = $(this).attr('action');
+    var params = $(this).serialize();
+    var callback = $(this).attr('ajax-callback');
+
+    $.post(action, params)
+      .done(function(response) { window[callback](jQuery.parseJSON(response)); })
+      .fail(function(response) { alert('something went wrong'); });
+
+    e.preventDefault();
+  });
+});
+
+function mandrill_connect_response(response) {
+  $('.mandrill-connection-status').html(response.message);
+
+  if (response.can_connect) {
+    toggle_tab('select_template');
+  }
+}
+
+function toggle_tab(name) {
+  $('#' + name + '_header').click();
+}
