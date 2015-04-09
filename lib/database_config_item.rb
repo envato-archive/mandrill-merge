85e95f5f220846f5e75c9a63d6dbf9bbb032f0e9
Glenn Tweedie <glenn.tweedie@envato.com>
2015-04-09 16:05:10 +0800

    get CA Cert file path in db connection settings ...and save them to the config use in connection string if applicable

--- lib/database_config_item.rb
+++ lib/database_config_item.rb
@@ -5,7 +5,9 @@ module Database
     end
 
     def connection_string
-      "#{driver}://#{username}:#{password}@#{host}:#{port}/#{database}"
+      "#{driver}://#{username}:#{password}@#{host}:#{port}/#{database}".tap do |conn_string|
+        conn_string << "?ssl[ca_cert]=#{ca_cert}" unless ca_cert.empty?
+      end
     end
 
     def empty?
@@ -19,10 +21,11 @@ module Database
       self.name     = settings['name']
       self.host     = settings['host']
       self.port     = settings['port']
-      self.ca_cert  = settings['ca_cert']
+      self.ca_cert  = settings['ca_cert'].to_s
       self.database = settings['database']
       self.password = settings['password']
       self.username = settings['username']
+      raise "CA CERT file not found" unless ca_cert.empty? || !File.exists?(ca_cert)
     end
   end
 end
